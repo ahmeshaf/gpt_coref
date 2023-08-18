9ed6cce0888dc5d746cdedc9f47af69220987c4d
rehan <ahmeshaf.rehan@gmail.com>
2023-08-18 14:42:09 -0600

    Update coreference experiments

--- project/scripts/coreference.py
+++ project/scripts/coreference.py
@@ -1,12 +1,24 @@
+import copy
+
 from coval.coval.conll.reader import get_coref_infos
 from coval.coval.eval.evaluator import evaluate_documents as evaluate
 from coval.coval.eval.evaluator import muc, b_cubed, ceafe, lea
 from prodigy.components.loaders import JSONL, JSON
+from scipy.sparse.csgraph import connected_components
+from scipy.sparse import csr_matrix, lil_matrix
+from typing import Optional
+from collections import defaultdict
+from tqdm import tqdm
 import numpy as np
+import itertools
+import timeit
+import typer
 import os
 
+app = typer.Typer()
 
 # ----------------------- Helpers ------------------------- #
+
 
 def generate_key_file(coref_map_tuples, name, out_dir, out_file_path):
     """
@@ -62,22 +74,372 @@ def run_coreference_results(gold_clusters, predicted_clusters, method_name):
     precision = np.round((mp + bp)/2, 1)
     connl = np.round((mf + bf + cf) / 3, 1)
 
-    print(f'{method_name} &&', recll, '&', precision, '&', connl, '&', lf, '\\\\')
+    print(f'{method_name} &&', recll, '&', precision, '&', connl, '\\\\')
 
 
-def single_ann_results(tasks_file_path):
+def c_clustering(sparse_matrix, m_ids):
+    _, labels = connected_components(sparse_matrix, return_labels=True)
+    return [(m, l) for m, l in zip(m_ids, labels)]
+
+
+def get_sparse_matrix(m2i, tasks):
+    eid2m_id = defaultdict(set)
+    for m_id, e_ids in tasks:
+        for e_id in e_ids:
+            eid2m_id[e_id].add(m_id)
+
+    row = []
+    col = []
+    data = []
+    for m_ids in eid2m_id.values():
+        for m1 in m_ids:
+            for m2 in m_ids:
+                if m1 in m2i and m2 in m2i:
+                    row.append(m2i[m1])
+                    col.append(m2i[m2])
+                    data.append(True)
+    n = len(m2i)
+    sparse_matrix = csr_matrix((data, (np.array(row), np.array(col))),
+                               shape=(n, n), dtype=np.int8)
+    return sparse_matrix
+
+
+def and_clustering(tasks_a, tasks_b):
+    """
+    Clusters are of the form (mention_id, EIDs)
+    :param tasks_a:
+    :param tasks_b:
+    :return:
+    """
+    m_ids_ordered = [m for m, _ in tasks_a]
+    m2i = {m: i for i, m in enumerate(m_ids_ordered)}
+
+    sparse_a = get_sparse_matrix(m2i, tasks_a)
+    sparse_b = get_sparse_matrix(m2i, tasks_b)
+
+    sparse_a_and_b = sparse_a.multiply(sparse_b)
+    return c_clustering(sparse_a_and_b, m_ids_ordered)
+
+
+def or_clustering(tasks_a, tasks_b):
+    m_ids_ordered = [m for m, _ in tasks_a]
+    m2i = {m: i for i, m in enumerate(m_ids_ordered)}
+
+    sparse_a = get_sparse_matrix(m2i, tasks_a)
+    sparse_b = get_sparse_matrix(m2i, tasks_b)
+
+    sparse_a_or_b = sparse_a + sparse_b
+    return c_clustering(sparse_a_or_b, m_ids_ordered)
+
+
+def a_clustering(tasks_a):
+    m_ids_ordered = [m for m, _ in tasks_a]
+    m2i = {m: i for i, m in enumerate(m_ids_ordered)}
+
+    sparse_a = get_sparse_matrix(m2i, tasks_a)
+    return c_clustering(sparse_a, m_ids_ordered)
+
+
+def add_eid(task, syn_map, sent_rs_id2task):
+    doc_id = task['doc_id']
+    sentence_id = task['sentence_id']
+    if 'EIDs' not in task:
+        task['EIDs'] = set()
+        arg0_rs_arg1s = itertools.product(task['arg0'], [task['roleset_id']], task['arg1'])
+        for arg0, rs, arg1 in arg0_rs_arg1s:
+            if len(syn_map) and rs in syn_map:
+                rs = syn_map[rs]
+            if (doc_id, sentence_id, arg1) in sent_rs_id2task:
+                add_eid(sent_rs_id2task[doc_id, sentence_id, arg1][0], syn_map, sent_rs_id2task)
+            eid_curr = (arg0, rs, arg1)
+
+            if (doc_id, sentence_id, arg1) not in sent_rs_id2task:
+                task['EIDs'].add(eid_curr)
+            else:
+                hop_eids = set(sent_rs_id2task[(doc_id, sentence_id, arg1)][0]['EIDs'])
+                for eid in hop_eids:
+                    for i in range(0, len(eid) - 2, 2):
+                        task['EIDs'].add((arg0, rs) + eid[i:])
+
+# def generate_eids(tasks, syn_map):
+
+
+def generate_eids(tasks, syn_map):
+    # generate the event identifiers of the individual event mentions by
+    # chaining nested event arguments
+    # single hop represents just the kb link of the arguments
+    for task in tasks:
+        for arg in ['arg0', 'arg1', 'argL', 'argT']:
+            task[arg] = task[arg].split('/')
+
+    for task in tasks:
+        rs_id = task['roleset_id']
+        if rs_id in syn_map:
+            rs_id = syn_map[rs_id]
+        task['EID_single'] = list(itertools.product([task['topic']], task['arg0'], [rs_id], task['arg1']))
+        task['EID_lt'] = list(itertools.product([task['topic']], task['arg0'], [rs_id], task['argL'], task['argT']))
+
+    sent_rs_id2task = {}
+    for task in tqdm(tasks, desc="generating eids"):
+        sent_rs_id = (task['doc_id'], task['sentence_id'], task['roleset_id'])
+        if sent_rs_id not in sent_rs_id2task:
+            sent_rs_id2task[sent_rs_id] = []
+        sent_rs_id2task[sent_rs_id].append(task)
+
+    for task in tasks:
+        add_eid(task, syn_map, sent_rs_id2task)
+
+
+imp_keys = ['arg0', 'arg1', 'argL', 'argT', 'roleset_id', 'lemma',
+            'doc_id', 'sentence_id', 'mention_id', 'gold_cluster', 'topic']
+
+
+def clean_task(task):
+    return {
+        k: task[k] for k in imp_keys
+    }
+
+
+def resolve_dict(key2cluster_arr):
+    key2cluster = {}
+    all_keys = sorted(list(key2cluster_arr.keys()))
+    key2i = {k: i for i, k in enumerate(all_keys)}
+    n = len(all_keys)
+    cluster2keys = {}
+    for key, cluster in key2cluster_arr.items():
+        for c in cluster:
+            if c not in cluster2keys:
+                cluster2keys[c] = []
+            cluster2keys[c].append(key)
+
+    key_mat = lil_matrix((n, n))
+
+    for cluster in cluster2keys.values():
+        for k1 in cluster:
+            for k2 in cluster:
+                key_mat[key2i[k1], key2i[k2]] = 1
+
+    adj_matrix = key_mat.tocsr()
+
+    # Find connected components
+    n_components, labels = connected_components(csgraph=adj_matrix, directed=False, return_labels=True)
+    for k, l in zip(all_keys, labels):
+        key2cluster[k] = l
+    return key2cluster
+
+
+def get_syn_map_vn(roleset_dict_path):
+    import pickle
+
+    pb_dict = pickle.load(open(roleset_dict_path, 'rb'))
+    rs2cluster_arr = {}
+    for rs, rs_dict in pb_dict.items():
+        if rs not in rs2cluster_arr:
+            rs2cluster_arr[rs] = []
+            for lexlink in rs_dict['lexlinks']:
+                if lexlink['@resource'] == 'VerbNet':
+                    rs2cluster_arr[rs].append(lexlink['@class'][0].split('.')[0])
+    syn_vn = resolve_dict(rs2cluster_arr)
+    return syn_vn
+
+# ---------------------- Commands ------------------------- #
+
+
+@app.command()
+def dummy():
+    pass
+
+
+@app.command()
+def run_stupidly_large_exp(tasks_file_path):
+    big_tasks_a = []
+    bit_tasks_b = []
+    tasks_a = list([clean_task(t) for t in JSONL(tasks_file_path)])
+    for i in range(100):
+        tasks__a = copy.deepcopy(tasks_a)
+        for t in tasks__a:
+            t['mention_id'] += '_' + str(i)
+            t['doc_id'] += '_' + str(i)
+            t['sentence_id'] += '_' + str(i)
+            # t['topic'] += '_' + str(i)
+        big_tasks_a.extend(tasks__a)
+        tasks__b = copy.deepcopy(tasks__a)
+        bit_tasks_b.extend(tasks__b)
+
+    print(len(big_tasks_a))
+
+    start_time = timeit.default_timer()
+    generate_eids(big_tasks_a, {})
+    eid_N_a = [(t['mention_id'], t['EIDs']) for t in big_tasks_a]
+    generate_eids(bit_tasks_b, {})
+    eid_N_b = [(t['mention_id'], t['EIDs']) for t in bit_tasks_b]
+
+    print('clustering')
+    eid_N_a_and_b = and_clustering(eid_N_a, eid_N_b)
+    elapsed = timeit.default_timer() - start_time
+
+    print('total elapsed time for the experiment', elapsed)
+
+
+@app.command()
+def and_ann_results(a1_path, a2_path, use_vn:Optional[bool]=False):
+    a1_tasks = list(JSONL(a1_path))
+    a2_tasks = list(JSONL(a2_path))
+
+    print(len(a1_tasks))
+    print(len(a2_tasks))
+
+    pb_syn_map = get_syn_map_vn('./outputs/common/roleset.dict')
+    if not use_vn:
+        pb_syn_map = {}
+
+    generate_eids(a1_tasks, pb_syn_map)
+    generate_eids(a2_tasks, pb_syn_map)
+    gold_clusters = [(t['mention_id'], t['gold_cluster']) for t in a1_tasks]
+
+    eid_N_1 = [(t['mention_id'], t['EIDs']) for t in a1_tasks]
+    eid_N_2 = [(t['mention_id'], t['EIDs']) for t in a2_tasks]
+
+    eid_N_1and2 = and_clustering(eid_N_1, eid_N_2)
+    eid_N = [(m_id, (str(c) + '_N',)) for m_id, c in eid_N_1and2]
+    run_coreference_results(gold_clusters, eid_N_1and2, '& \\eidNH')
+
+    eid_lts_1 = [(t['mention_id'], t['EID_lt']) for t in a1_tasks]
+    eid_lts_2 = [(t['mention_id'], t['EID_lt']) for t in a2_tasks]
+
+    eid_lt_clus = and_clustering(eid_lts_1, eid_lts_2)
+    eid_lts = [(m_id, (str(c) + '_LT',)) for m_id, c in eid_lt_clus]
+    run_coreference_results(gold_clusters, eid_lt_clus, '& \\eidLTH')
+
+    eid_N_clus_and_lt = and_clustering(eid_N, eid_lts)
+    run_coreference_results(gold_clusters, eid_N_clus_and_lt, '& \\eidNH~\\AND~\\eidLTH')
+
+    eid_N_clus_or_lt = or_clustering(eid_N, eid_lts)
+    run_coreference_results(gold_clusters, eid_N_clus_or_lt, '& \\eidNH~\\OR~\\eidLTH')
+
+
+@app.command()
+def or_ann_results(a1_path, a2_path, use_vn: Optional[bool]=False):
+    a1_tasks = list(JSONL(a1_path))
+    a2_tasks = list(JSONL(a2_path))
+
+    print(len(a1_tasks))
+    print(len(a2_tasks))
+
+    pb_syn_map = get_syn_map_vn('./outputs/common/roleset.dict')
+    if not use_vn:
+        pb_syn_map = {}
+
+    generate_eids(a1_tasks, pb_syn_map)
+    generate_eids(a2_tasks, pb_syn_map)
+    gold_clusters = [(t['mention_id'], t['gold_cluster']) for t in a1_tasks]
+
+    eid_N_1 = [(t['mention_id'], t['EIDs']) for t in a1_tasks]
+    eid_N_2 = [(t['mention_id'], t['EIDs']) for t in a2_tasks]
+
+    eid_N_1or2 = or_clustering(eid_N_1, eid_N_2)
+    eid_N = [(m_id, (str(c) + '_N',)) for m_id, c in eid_N_1or2]
+    run_coreference_results(gold_clusters, eid_N_1or2, '& \\eidNH')
+
+    eid_lts_1 = [(t['mention_id'], t['EID_lt']) for t in a1_tasks]
+    eid_lts_2 = [(t['mention_id'], t['EID_lt']) for t in a2_tasks]
+
+    eid_lt_clus = or_clustering(eid_lts_1, eid_lts_2)
+    eid_lts = [(m_id, (str(c) + '_LT',)) for m_id, c in eid_lt_clus]
+    run_coreference_results(gold_clusters, eid_lt_clus, '& \\eidLTH')
+
+    eid_N_clus_and_lt = and_clustering(eid_N, eid_lts)
+    run_coreference_results(gold_clusters, eid_N_clus_and_lt, '& \\eidNH~\\AND~\\eidLTH')
+
+    eid_N_clus_or_lt = or_clustering(eid_N, eid_lts)
+    run_coreference_results(gold_clusters, eid_N_clus_or_lt, '& \\eidNH~\\OR~\\eidLTH')
+
+
+@app.command()
+def single_ann_results(tasks_file_path, use_vn: Optional[bool]=False):
     tasks = list(JSONL(tasks_file_path))
+
+    tasks = [{k: t[k] for k in imp_keys} for t in tasks]
+
+    pb_syn_map = {}
+    if use_vn:
+        pb_syn_map = get_syn_map_vn('./outputs/common/roleset.dict')
+    print(len(pb_syn_map))
+    print(len(tasks))
+    generate_eids(tasks, pb_syn_map)
 
     gold_clusters = [(t['mention_id'], t['gold_cluster']) for t in tasks]
     # Lemma-Only
     pred_clusters = [(t['mention_id'], t['lemma']) for t in tasks]
-    run_coreference_results(gold_clusters, pred_clusters, 'Lemma-Only')
+    run_coreference_results(gold_clusters, pred_clusters, '\\LEM')
 
     # RS-ONLY
     pred_clusters = [(t['mention_id'], t['roleset_id']) for t in tasks]
-    run_coreference_results(gold_clusters, pred_clusters, 'RS-Only')
+    run_coreference_results(gold_clusters, pred_clusters, '& \\RSHum')
+
+    if pb_syn_map and len(pb_syn_map):
+        pred_clusters = [(t['mention_id'],
+                          pb_syn_map[t['roleset_id']] if t['roleset_id'] in pb_syn_map else t['roleset_id']
+                          ) for t in tasks]
+        run_coreference_results(gold_clusters, pred_clusters, '& \\PBHVN')
+
+    # EID-0-hop
+    eid_0s =[(t['mention_id'], t['EID_single']) for t in tasks]
+    eid_0_clus = a_clustering(eid_0s)
+    # run_coreference_results(gold_clusters, eid_0_clus, 'EID-0-hop')
+
+    # eid-0 and eid_lt
+    # eid_0_and_lt = and_clustering(eid_0s, eid_lts)
+    # run_coreference_results(gold_clusters, eid_0_and_lt, 'EID_0_and_lt')
+
+    # eid-0 or eid_lt
+    # eid_0_or_lt = or_clustering(eid_0s, eid_lts)
+    # run_coreference_results(gold_clusters, eid_0_or_lt, 'EID_0_or_lt')
+
+    # eid-N
+    # generate_eids(tasks)
+    eid_N = [(t['mention_id'], t['EIDs']) for t in tasks]
+    eid_N_clus = a_clustering(eid_N)
+    run_coreference_results(gold_clusters, eid_N_clus, '& \\eidNH')
+
+    eid_lts = [(t['mention_id'], t['EID_lt']) for t in tasks]
+    eid_lt_clus = a_clustering(eid_lts)
+    run_coreference_results(gold_clusters, eid_lt_clus, '& \\eidLTH')
+
+    eid_N_clus_and_lt = and_clustering(eid_N, eid_lts)
+    run_coreference_results(gold_clusters, eid_N_clus_and_lt, '& \\eidNH~\\AND~\\eidLTH')
+
+    eid_N_clus_or_lt = or_clustering(eid_N, eid_lts)
+    run_coreference_results(gold_clusters, eid_N_clus_or_lt, '& \\eidNH~\\OR~\\eidLTH')
+
+    # eid_N_clus_or_0 = and_clustering(eid_N, eid_0s)
+    # run_coreference_results(gold_clusters, eid_N_clus_or_0, 'eid_N_and_0')
+
+
+@app.command()
+def gpt_results(tasks_file_path, use_vn: Optional[bool]=False):
+    tasks = list(JSON(tasks_file_path))
+    gold_clusters = [(t['mention_id'], t['gold_cluster']) for t in tasks]
+    # Lemma-Only
+    pred_clusters = [(t['mention_id'], t['roleset_id']) for t in tasks]
+    run_coreference_results(gold_clusters, pred_clusters, '\\RSGPT')
+
+    generate_eids(tasks, {})
+
+    eid_N = [(t['mention_id'], t['EIDs']) for t in tasks]
+    eid_N_clus = a_clustering(eid_N)
+    run_coreference_results(gold_clusters, eid_N_clus, '& \\eidNH')
+
+    eid_lts = [(t['mention_id'], t['EID_lt']) for t in tasks]
+    eid_lt_clus = a_clustering(eid_lts)
+    run_coreference_results(gold_clusters, eid_lt_clus, '& \\eidLTH')
+
+    eid_N_clus_and_lt = and_clustering(eid_N, eid_lts)
+    run_coreference_results(gold_clusters, eid_N_clus_and_lt, '& \\eidNH~\\AND~\\eidLTH')
+
+    eid_N_clus_or_lt = or_clustering(eid_N, eid_lts)
+    run_coreference_results(gold_clusters, eid_N_clus_or_lt, '& \\eidNH~\\OR~\\eidLTH')
 
 
 if __name__ == '__main__':
-    dev_ann1 = '../annotations/ecb/dev/dev_full_rs_ann1.jsonl'
-    single_ann_results(dev_ann1)
+    app()
--- project/scripts/gpt_pe.py
+++ project/scripts/gpt_pe.py
@@ -2,16 +2,19 @@ from spacy.util import ensure_path
 from typing import Optional
 from pathlib import Path
 from tqdm import tqdm
+import lexnlp.extract.en.dates
 import openai
 import pickle
+import lexnlp
 import typer
 import json
 import time
 import os
+import re
 
 
 curr_file_dir = os.path.dirname(__file__)
-PROMPT_FILE_SIMPLE = curr_file_dir + '/prompt.amr.txt'
+PROMPT_FILE_SIMPLE = curr_file_dir + "/prompt.amr.txt"
 app = typer.Typer()
 
 COM_SENTENCE_PLACEHOLDER = "sentence: SENTENCE_VAL\nyour response:\n"
@@ -21,6 +24,61 @@ SENTENCE_VAL = "SENTENCE_VAL"
 DOC_VAL = "DOC_VAL"
 
 # -------------------- Helpers --------------------- #
+
+
+def get_dates(text):
+    return list(lexnlp.extract.en.dates.get_dates(text))
+
+
+class Argument:
+    def __init__(self, line):
+        self.arg_name = line.split(":")[0]
+        arg_value = "".join([s.strip() for s in line.split(":")[1:]])
+        self.parse_arg_value(arg_value)
+        self.arg_text = ""
+        self.arg_reference = ""
+        self.wiki = None
+        self.non_wiki = None
+        self.rolesets = None
+        self.dates = None
+        self.args_multi = None
+
+    def parse_arg_value(self, text):
+        self.arg_text = text.split("(")[0].strip()
+        self.arg_reference = (
+            text.split("(")[-1].strip(")").replace("referencing", "").strip()
+        )
+
+        self.wiki = [
+            w.split("wiki/")[-1].split("#")[0]
+            for w in re.findall(r"(wiki/\S+)", self.arg_reference)
+        ]
+        self.non_wiki = [
+            s.replace("https//", "")
+            .replace("http//", "")
+            .replace("www.", "")
+            .strip(".com")
+            for s in re.findall(r"(https?:?//\S+)", self.arg_reference)
+            if "wikipedia" not in s
+        ]
+        self.rolesets = re.findall(r".*\.\d{1,3}", text)
+
+        self.dates = list(lexnlp.extract.en.dates.get_dates(text))
+        self.args_multi = [s.strip() for s in self.arg_text.split("and")]
+
+    def __str__(self):
+        return " ".join(
+            [
+                f"[{self.arg_name}]",
+                f"[{self.arg_text}]",
+                f"[args: {self.args_multi}]"
+                f"[dates: {self.dates}]"
+                f"[rolesets: {self.rolesets}]",
+                f"[non-wiki: {self.non_wiki}]",
+                f"[wiki: {self.wiki}]",
+                f"[{self.arg_reference}]",
+            ]
+        )
 
 
 def _prepare(mention_map_path, mention_ids_path, output_path, cache_file, restart):
@@ -36,11 +94,11 @@ def _prepare(mention_map_path, mention_ids_path, output_path, cache_file, restar
         if cache_file.exists() and restart:
             os.remove(cache_file)
         elif cache_file.exists():
-            gpt_cached_responses = pickle.load(open(cache_file, 'rb'))
+            gpt_cached_responses = pickle.load(open(cache_file, "rb"))
         if not cache_file.parent.exists():
             cache_file.parent.mkdir(parents=True)
 
-    mention_map = pickle.load(open(mention_map_path, 'rb'))
+    mention_map = pickle.load(open(mention_map_path, "rb"))
     with open(mention_ids_path) as mff:
         mention_ids = [line.strip() for line in mff]
 
@@ -48,17 +106,17 @@ def _prepare(mention_map_path, mention_ids_path, output_path, cache_file, restar
 
 
 def _gpt_response2dict(gpt_response):
-    choice = gpt_response['choices'][0]
+    choice = gpt_response["choices"][0]
     return {
-        'usage': gpt_response['usage'],
-        'choices': [
+        "usage": gpt_response["usage"],
+        "choices": [
             {
-                'message': {
-                    'content': choice['message']['content'],
-                    'role': choice['message']['role']
+                "message": {
+                    "content": choice["message"]["content"],
+                    "role": choice["message"]["role"],
                 }
             }
-        ]
+        ],
     }
 
 
@@ -66,14 +124,12 @@ def _run_gpt_with_prompt(prompt, model_name):
     messages = []
     message = {"role": "user", "content": prompt}
     messages.append(message)
-    chat_comp = openai.ChatCompletion.create(
-        model=model_name,
-        messages=messages
-    )
+    chat_comp = openai.ChatCompletion.create(model=model_name, messages=messages)
     response = chat_comp
     response = json.dumps(response, indent=2)
     response = json.loads(response)
     return response
+
 
 # -------------------- Commands -------------------- #
 
@@ -84,54 +140,192 @@ def dummy():
 
 
 @app.command()
-def gpt_simple(
-        mention_map_path: Path,
-        mention_ids_path: Path,
-        output_path: Path,
-        model_name: str,
-        cache_file: Optional[Path] = None,
-        restart: Optional[bool] = False
+def gpt_simple_ecb(
+    mention_map_path: Path,
+    mention_ids_path: Path,
+    output_path: Path,
+    split: str,
+    model_name: str,
+    cache_file: Optional[Path] = None,
+    restart: Optional[bool] = False,
 ):
-    mention_map, mention_ids, gpt_responses = _prepare(mention_map_path, mention_ids_path, output_path, cache_file, restart)
+    mention_map, mention_ids, gpt_responses = _prepare(
+        mention_map_path, mention_ids_path, output_path, cache_file, restart
+    )
+
+    mention_ids = [
+        key
+        for key, val in mention_map.items()
+        if val["split"] == split and val["men_type"] == "evt"
+    ]
+
+    print(len(mention_ids))
 
     with open(PROMPT_FILE_SIMPLE) as pff:
         prompt_ph = pff.read()
 
-    pbar = tqdm(total=len(mention_ids), desc='running llm')
+    pbar = tqdm(total=len(mention_ids), desc="running llm")
 
     while len(mention_ids):
         m_id = mention_ids[0]
         mention = mention_map[m_id]
-        marked_doc = mention['bert_doc']
-        marked_sentence = mention['bert_sentence']
+        marked_doc = mention["bert_doc"]
+        marked_sentence = mention["bert_sentence"]
 
         my_prompt = prompt_ph.replace(DOC_VAL, marked_doc)
         comp_prompt = COM_SENTENCE_PLACEHOLDER.replace(SENTENCE_VAL, marked_sentence)
-        comp_prompt_evt = COM_SENTENCE_EVT_PLACEHOLDER.replace(SENTENCE_VAL, marked_sentence)
+        comp_prompt_evt = COM_SENTENCE_EVT_PLACEHOLDER.replace(
+            SENTENCE_VAL, marked_sentence
+        )
 
-        my_prompt_reg = my_prompt + comp_prompt
-        my_prompt_evt = my_prompt + comp_prompt_evt
-
+        my_prompt = prompt_ph + "\nSentence: " + marked_sentence
+        # print(my_prompt)
         try:
             if m_id not in gpt_responses:
-                response_reg = _run_gpt_with_prompt(my_prompt_reg, model_name)
-                gpt_responses[m_id] = {'regular': {'prompt': my_prompt_reg, 'response': response_reg}}
+                response_evt = _run_gpt_with_prompt(my_prompt, model_name)
+                gpt_responses[m_id] = {"prompt": my_prompt, "response": response_evt}
 
-                response_evt = _run_gpt_with_prompt(my_prompt_evt, model_name)
-                gpt_responses[m_id]['eventive'] = {'prompt': my_prompt_evt, 'response': response_evt}
-
-                pickle.dump(gpt_responses, open(cache_file, 'wb'))
+                pickle.dump(gpt_responses, open(cache_file, "wb"))
         except openai.error.RateLimitError:
-            print('Rate limit reached. sleeping')
+            print("Rate limit reached. sleeping")
             time.sleep(15)  # adding delay to avoid time-out
+        except openai.error.APIError:
+            print("APIError. sleeping")
+            time.sleep(15)  # adding delay to avoid time-out
+        except openai.error.APIConnectionError:
+            print("APIConnectionError. sleeping")
+            time.sleep(15)
+        except openai.error.ServiceUnavailableError:
+            print("ServiceUnavailableError. sleeping")
+            time.sleep(15)
 
         if m_id in gpt_responses:
+            # print(gpt_responses[m_id])
+            response = gpt_responses[m_id]["response"]["choices"][0]["message"][
+                "content"
+            ]
+            print(response)
             pbar.update(1)
             mention_ids.pop(0)
-        # gpt_response = response['content']
+    json.dump(gpt_responses, open(output_path, "w"))
 
 
-if __name__ == '__main__':
+@app.command()
+def parse_gpt_responses(input_path: Path, output_path: Path, mention_map_path: Path):
+    mention_map = pickle.load(open(mention_map_path, "rb"))
+    gpt_responses = json.load(open(input_path))
+
+    arg_map = {"ARG-0": "arg0", "ARG-1": "arg1", "ARG-Loc": "argL", "ARG-Time": "argT"}
+
+    def get_wikis(string):
+        all_wikis = [w.split("wiki/")[-1] for w in re.findall(r"(wiki/\S+)", string)]
+        all_wikis_with_split = set()
+        for wik in all_wikis:
+            all_wikis_with_split.add(wik)
+            all_wikis_with_split.add(wik.split("#")[0])
+        return all_wikis_with_split
+
+    parsed_responses = []
+    for m_id, g_res in gpt_responses.items():
+        m_parsed = {}
+        mention = mention_map[m_id]
+        mention["mention_id"] = m_id
+        gpt_response = g_res["response"]["choices"][0]["message"]["content"]
+        lines = gpt_response.split("\n")
+        for line in lines:
+            if line.startswith("Roleset"):
+                # print(gpt_response)
+                # print(line)
+                m_parsed["roleset_id"] = line[line.index(":") :].strip()
+                m_parsed["roleset_id"] = mention["lemma"]
+            if line.startswith("ARG"):
+                arg_name = line[: line.index(":")]
+                arg_val = line[line.index(":") :].strip()
+                wikis = get_wikis(arg_val)
+                if len(wikis) == 0:
+                    if "(" in arg_val:
+                        arg_val = arg_val[: arg_val.index("(")].strip('"').strip()
+                else:
+                    arg_val = "/".join(wikis)
+
+                if arg_name.startswith("ARG-Time"):
+                    date_vals = get_dates(arg_val)
+                    if len(date_vals):
+                        arg_val = "/".join([str(d.year) for d in date_vals])
+
+                for val in arg_map.values():
+                    m_parsed[val] = "<NA>"
+                if arg_name in arg_map:
+                    m_parsed[arg_map[arg_name]] = arg_val
+
+        for k in ["mention_id", "doc_id", "sentence_id", "topic", "gold_cluster"]:
+            m_parsed[k] = mention[k]
+        parsed_responses.append(m_parsed)
+    json.dump(parsed_responses, open(output_path, "w"))
+
+
+@app.command()
+def gpt_simple(
+    mention_map_path: Path,
+    mention_ids_path: Path,
+    output_path: Path,
+    model_name: str,
+    cache_file: Optional[Path] = None,
+    restart: Optional[bool] = False,
+):
+    mention_map, mention_ids, gpt_responses = _prepare(
+        mention_map_path, mention_ids_path, output_path, cache_file, restart
+    )
+
+    with open(PROMPT_FILE_SIMPLE) as pff:
+        prompt_ph = pff.read()
+
+    pbar = tqdm(total=len(mention_ids), desc="running llm")
+
+    while len(mention_ids):
+        m_id = mention_ids[0]
+        mention = mention_map[m_id]
+        marked_doc = mention["bert_doc"]
+        marked_sentence = mention["bert_sentence"]
+
+        my_prompt = prompt_ph.replace(DOC_VAL, marked_doc)
+        comp_prompt = COM_SENTENCE_PLACEHOLDER.replace(SENTENCE_VAL, marked_sentence)
+        comp_prompt_evt = COM_SENTENCE_EVT_PLACEHOLDER.replace(
+            SENTENCE_VAL, marked_sentence
+        )
+
+        my_prompt = prompt_ph + "\nSentence: " + marked_sentence
+        # print(my_prompt)
+        try:
+            if m_id not in gpt_responses:
+                response_evt = _run_gpt_with_prompt(my_prompt, model_name)
+                gpt_responses[m_id] = {"prompt": my_prompt, "response": response_evt}
+
+                pickle.dump(gpt_responses, open(cache_file, "wb"))
+        except openai.error.RateLimitError:
+            print("Rate limit reached. sleeping")
+            time.sleep(15)  # adding delay to avoid time-out
+        except openai.error.APIError:
+            print("APIError. sleeping")
+            time.sleep(15)  # adding delay to avoid time-out
+        except openai.error.APIConnectionError:
+            print("APIConnectionError. sleeping")
+            time.sleep(15)
+        except openai.error.ServiceUnavailableError:
+            print("ServiceUnavailableError. sleeping")
+            time.sleep(15)
+
+        if m_id in gpt_responses:
+            # print(gpt_responses[m_id])
+            response = gpt_responses[m_id]["response"]["choices"][0]["message"][
+                "content"
+            ]
+            print(response)
+            pbar.update(1)
+            mention_ids.pop(0)
+    # gpt_response = response['content']
+    json.dump(gpt_responses, open(output_path, "w"))
+
+
+if __name__ == "__main__":
     app()
-
-
