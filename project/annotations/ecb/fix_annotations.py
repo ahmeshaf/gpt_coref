ede1dfb187a42740b5ee138ac42b4d1efde3eae7
rehan <ahmeshaf.rehan@gmail.com>
2023-09-01 16:18:45 -0600

    newline to <p/>

--- project/annotations/ecb/fix_annotations.py
+++ project/annotations/ecb/fix_annotations.py
@@ -8,17 +8,17 @@ if __name__ == "__main__":
     anno_dir = sys.argv[1]
     mention_map_file = sys.argv[2]
 
-    mention_map = pickle.load(open(mention_map_file, 'rb'))
+    mention_map = pickle.load(open(mention_map_file, "rb"))
 
     for file_path in glob.glob(anno_dir + "/*/*.json"):
         annos = json.load(open(file_path))
 
         for task in annos:
-            task['marked_sentence'] = mention_map[task['mention_id']]['marked_sentence']
-            task['marked_doc'] = mention_map[task['mention_id']]['marked_doc']
+            task["marked_sentence"] = mention_map[task["mention_id"]]["marked_sentence"]
+            marked_doc = mention_map[task["mention_id"]]["marked_doc"]
+            task["marked_doc"] = marked_doc.replace('\n', ' <p/>')
 
-            if 'bert_doc' in task:
-                task.pop('bert_doc')
+            if "bert_doc" in task:
+                task.pop("bert_doc")
 
-        json.dump(annos, open(file_path, 'w'), indent=1)
-
+        json.dump(annos, open(file_path, "w"), indent=1)
